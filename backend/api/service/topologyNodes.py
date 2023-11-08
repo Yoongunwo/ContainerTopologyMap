abb4a76cf32fce76d0ee53cf4dd23d43e5036a93
Gunwo Yoon <glove2275@pusan.ac.kr>
2023-11-08 21:59:37 +0900

    fix topologyNodes.py to give more imformation to front

--- backend/api/service/topologyNodes.py
+++ backend/api/service/topologyNodes.py
@@ -10,20 +10,18 @@ def getNodes(ssh_client : paramiko.SSHClient):
     nodesInfo = nodesInfo.split('\n')
     nodesInfo = nodesInfo[1:-1]
     nodesInfo = [re.split(r'\s{2,}', node) for node in nodesInfo]
-    nodesInfo = [{'name': node[0], 'ip': node[5], 'status': node[1], 
-                'roles': node[2], 'os-image': node[7],
-                'children':[{'name': 'pod', 'children': []}], 'kind':'node'}
-                for node in nodesInfo]
+    nodesInfo = [{'name': node[0], 'status': node[1], 'roles': node[2], 'age': node[3],
+                'kubernetes version': node[4], 'ip': node[5], 'os-image': node[7], 'kind':'node',
+                'children':[{'name': 'pod', 'children': []}]} for node in nodesInfo]
 
     stdin, stdout, stderr = ssh_client.exec_command('kubectl get pods --all-namespaces -o wide')  # 예: 원하는 명령어 입력
     podsInfo = stdout.read().decode('utf-8')
     podsInfo = podsInfo.split('\n')
     podsInfo = podsInfo[1:-1]
     podsInfo = [re.split(r'\s{2,}', pod) for pod in podsInfo]
-    podsInfo = [{'namespace': pod[0], 'name': pod[1], 'ready': pod[2],
-                'status': pod[3], 'ip': pod[6], 
-                'node':pod[7], 'kind':'pod'} 
-                for pod in podsInfo]
+    podsInfo = [{'namespace': pod[0], 'name': pod[1], 'ready': pod[2], 
+                'status': pod[3], 'restarts': pod[4], 'age': pod[5],
+                'ip': pod[6], 'node':pod[7], 'kind': 'pod'} for pod in podsInfo]  
 
 
     for pod in podsInfo:
--- syscall.py
+++ syscall.py
@@ -18,16 +18,19 @@ nodesInfo = stdout.read().decode('utf-8')
 nodesInfo = nodesInfo.split('\n')
 nodesInfo = nodesInfo[1:-1]
 nodesInfo = [re.split(r'\s{2,}', node) for node in nodesInfo]
-nodesInfo = [{'name': node[0], 'ip': node[5], 'status': node[1], 
-              'roles': node[2], 'os-image': node[7],
-            'children':[{'name': 'pod', 'childen': []}]} for node in nodesInfo]
+nodesInfo = [{'name': node[0], 'status': node[1], 'roles': node[2], 'age': node[3],
+            'kubernetes version': node[4], 'ip': node[5], 'os-image': node[7], 'kind':'node',
+            'children':[{'name': 'pod', 'children': []}]} for node in nodesInfo]
 
 stdin, stdout, stderr = ssh_client.exec_command('kubectl get pods --all-namespaces -o wide')  # 예: 원하는 명령어 입력
 podsInfo = stdout.read().decode('utf-8')
+print(podsInfo)
 podsInfo = podsInfo.split('\n')
 podsInfo = podsInfo[1:-1]
 podsInfo = [re.split(r'\s{2,}', pod) for pod in podsInfo]
-podsInfo = [{'namespace': pod[0], 'name': pod[1], 'ready': pod[2], 'status': pod[3], 'ip': pod[6], 'node':pod[7]} for pod in podsInfo]
+podsInfo = [{'namespace': pod[0], 'name': pod[1], 'ready': pod[2], 
+             'status': pod[3], 'restarts': pod[4], 'age': pod[5],
+             'ip': pod[6], 'node':pod[7], 'kind': 'pod'} for pod in podsInfo]
 
 
 for pod in podsInfo:
